728a62abe0f39a3552ba7c2afc79eb35591834f4
RaviKaran Srivastava <58618023+1710ravi@users.noreply.github.com>
2020-01-16 00:09:29 +0530

    Delete myObject.js

--- myObject.js
+++
@@ -1,6 +0,0 @@
-// let myYoutubeVideo = {
-//     title: 'JavaScript',
-//     time: 30,
-//     videoCreator: 'Ravikaran'
-// }
-// console.log(`hey new video on ${myYoutubeVideo.title} created by ${myYoutubeVideo.videoCreator}`);
