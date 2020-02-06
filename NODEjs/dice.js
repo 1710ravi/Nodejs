d2e6665e9a4f52f0744c73d9769961dd5b7ce006
RaviKaran Srivastava <58618023+1710ravi@users.noreply.github.com>
2020-02-06 21:48:06 +0530

    Update dice.js

--- NODEjs/dice.js
+++ NODEjs/dice.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // let numOne = 22;
 // let numTwo = 7;
 // result = 22/7;
@@ -11,17 +11,3 @@ let upper = 25;
 let lower = 20 ;
 yo = Math.floor(Math.random()*(upper-lower+1)+lower);
 console.log(yo);
-=======
-// let numOne = 22;
-// let numTwo = 7;
-// result = 22/7;
-// console.log(result.toFixed(2));
-// let yo = result.toFixed(2);
-// console.log(Math.ceil(yo));
-// console.log(Math.floor(yo));
-
-let upper = 25;
-let lower = 20 ;
-yo = Math.floor(Math.random()*(upper-lower+1)+lower);
-console.log(yo);
->>>>>>> 17c9752c757c3948407c8d272914859cf8bfbf6b
