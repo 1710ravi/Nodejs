19eac8e1282bd7183e39f22aa2ab7ca855d5c4b0
RaviKaran Srivastava <58618023+1710ravi@users.noreply.github.com>
2020-02-06 21:46:19 +0530

    Update webcheck.js

--- NODEjs/webcheck.js
+++ NODEjs/webcheck.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let userEmail = 'ravikaran123';
 let password = '1234';
  
@@ -13,20 +13,5 @@ let passChecker = function(pass){
 }}
 console.log(userChecker(userEmail));
 
-=======
-let userEmail = 'ravikaran123';
-let password = '1234';
- 
-let userChecker = function(mystring){
-    if ((mystring.includes(123)) && (mystring.length > 6)) {
-        return true  
-}}
 
-let passChecker = function(pass){
-    if ((pass.includes(123)) && (pass.length < 5)) {
-        return true
-}}
-console.log(userChecker(userEmail));
-
->>>>>>> 17c9752c757c3948407c8d272914859cf8bfbf6b
 console.log(passChecker(password));
