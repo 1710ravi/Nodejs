31b0e541a9ee25664144c85bb4d85fc9c2d159bb
RaviKaran Srivastava <58618023+1710ravi@users.noreply.github.com>
2020-02-06 21:47:39 +0530

    Update assign.js

--- NODEjs/assign.js
+++ NODEjs/assign.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let myGrade = function(marks,totalMarks){
     let myPercent = (marks/totalMarks)*100;
     let Got = '';
@@ -14,20 +14,3 @@ let myGrade = function(marks,totalMarks){
 let yourResult = myGrade(92,100);
 console.log(yourResult);
 
-=======
-let myGrade = function(marks,totalMarks){
-    let myPercent = (marks/totalMarks)*100;
-    let Got = '';
-        if (myPercent>=90){
-        Got = 'A';}
-        else if(myPercent>=80){
-        Got = 'B';}
-        else{
-        Got = 'C';}
-
-        return Got;
-}
-let yourResult = myGrade(92,100);
-console.log(yourResult);
-
->>>>>>> 17c9752c757c3948407c8d272914859cf8bfbf6b
